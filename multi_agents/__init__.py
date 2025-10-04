7984e721fcc49cf3fe555434b57ff0ca92777b96
tungbi811 <tungduynguyen811@gmail.com>
2025-10-04 18:55:55 +1000

    update data scientist

--- multi_agents/__init__.py
+++ multi_agents/__init__.py
@@ -2,7 +2,7 @@ from multi_agents.business_analyst import BusinessAnalyst
 from multi_agents.data_analyst import DataAnalyst
 from multi_agents.data_engineer import DataEngineer
 from multi_agents.data_engineer import DataEngineer
-from multi_agents.modeler import Modeler
+from multi_agents.data_scientist import Modeler
 from multi_agents.business_translator import BusinessTranslator
 from multi_agents.coder import Coder
 
